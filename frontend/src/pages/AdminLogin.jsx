f27c5b5682b2c7060eb47bcae0aab46981125350
davidmassmutual <your-email@example.com>
2025-12-12 17:15:50 +0100

    done

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { toast } from 'react-toastify';
 import { useAuth } from '../context/AuthContext';
 import '../styles/AdminLogin.css';
 import API_BASE_URL from '../config/api';
